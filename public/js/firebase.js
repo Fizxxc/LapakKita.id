29e016f2b4e9a1103e4efb48a73c8ae088e46afb
Fizxxc <fizzx909@gmail.com>
2025-08-28 08:10:31 +0700

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -23,14 +23,14 @@ import {
 // Konfigurasi Firebase
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAN40NJ04QR03uF54k3rQGph81kEhKthtM",
-  authDomain: "lapakkita.id",
-  databaseURL:"https://lapakkita-8a21d-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  projectId: "lapakkita-8a21d",
-  storageBucket: "lapakkita-8a21d.firebasestorage.app",
-  messagingSenderId: "961001666741",
-  appId: "1:961001666741:web:01925c86163780bc93e6fc",
-  measurementId: "G-N9T7CQ59S3"
+    apiKey: "AIzaSyAN40NJ04QR03uF54k3rQGph81kEhKthtM",
+    authDomain: "lapakkita.id",
+    databaseURL: "https://lapakkita-8a21d-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "lapakkita-8a21d",
+    storageBucket: "lapakkita-8a21d.firebasestorage.app",
+    messagingSenderId: "961001666741",
+    appId: "1:961001666741:web:01925c86163780bc93e6fc",
+    measurementId: "G-N9T7CQ59S3"
 };
 
 // Inisialisasi Firebase
